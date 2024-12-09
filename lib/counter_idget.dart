4fed1f6d3a49facb5c20868b3f2ef03a197d5f70
zisanturan <zisan3003@gmail.com>
2024-12-09 15:54:22 +0300

    x
    x

--- lib/counter_idget.dart
+++ lib/counter_idget.dart
@@ -1,24 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Test için örnek bir widget oluştur
-    await tester.pumpWidget(MaterialApp(home: CounterWidget()));
-
-    // Sayacın başlangıçta 0 olduğunu doğrula
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // '+' butonuna tıkla ve yeniden oluştur
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Sayacın 1 olduğunu doğrula
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
 
 class CounterWidget extends StatefulWidget {
   const CounterWidget({super.key});
