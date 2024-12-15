b28a0abd57d7e63cb466437956879687edc077b0
zisanturan <zisan3003@gmail.com>
2024-12-15 23:16:28 +0300

    z

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart'; // Crashlytics paketini ekledik
+// Kullanıcı giriş/kayıt ekranı
+// Kullanıcı giriş yaptıktan sonra ana ekran
+import 'screens/splash_screen.dart'; // SplashScreen için yeni bir dosya
 import 'firebase_options.dart'; // Firebase yapılandırma dosyası
-// SplashScreen için yeni bir dosya
-import 'screens/error_screen.dart'; // Hata ekranı
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Firebase'i başlatmak ve Crashlytics'i ayarlamak için asenkron işlemi başlatıyoruz
   try {
+    // Firebase'i başlat
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform, // Firebase yapılandırması
     );
-
-    // Crashlytics için hata yakalama
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   } catch (e) {
-    // Firebase başlatılamazsa, hata mesajı göster ve uygulamayı ErrorScreen ile başlat
+    // Firebase başlatılamazsa hata mesajı göster
     if (kDebugMode) {
       print("Firebase bağlantısı kurulamadı: $e");
     }
-    runApp(MyApp(errorMessage: 'Firebase bağlantısı kurulamadı.'));
     return;
   }
-
-  // Firebase başarıyla başlatılırsa, SplashScreen'i göster
-  runApp(const MyApp(errorMessage: ''));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String errorMessage;
-
-  const MyApp({super.key, required this.errorMessage});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -43,49 +34,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue, // Uygulama teması mavi tonlarıyla
       ),
-      home: errorMessage.isEmpty
-          ? const SplashScreen() // Firebase başarıyla başlatıldıysa SplashScreen'i göster
-          : ErrorScreen(errorMessage: errorMessage), // Hata durumunda ErrorScreen'i göster
-    );
-  }
-}
-
-// Firebase Crashlytics ile test hatası raporlama
-void simulateCrash() {
-  FirebaseCrashlytics.instance.recordError(
-    Exception("Simulated crash"),
-    StackTrace.current,
-    reason: "test hatası",
-  );
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TSB Uygulaması'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Firebase Crashlytics Test',
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Simüle edilmiş hata fonksiyonunu tetikleyin
-                simulateCrash();
-              },
-              child: const Text('Hata Simüle Et'),
-            ),
-          ],
-        ),
-      ),
+      home: const SplashScreen(), // İlk olarak SplashScreen gösterilecek
     );
   }
 }
